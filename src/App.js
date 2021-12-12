23c51bbce0848cfbe6a60f04c4ee1ea0f7ee961d
diogosamtos <diogo.santos@geowellex.com>
2021-12-12 06:55:40 -0300

    Changing api connection logic

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
 import { useEffect, useState } from "react"
 import axios from 'axios';
 import './app.css';
+import pokeball from "./images/pokeball-red.svg";
 
 
 function App() {
 
   const [Pokemons, setPokemons] = useState([]);
-  const [Current, setCurrent] = useState({});
+  const [Current, setCurrent] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`).then(response => {
-      setPokemons(response.data.results);
-    });
+    if(Object.keys(Pokemons).length < 151){
+      for (let i = 1; i < 151; i++) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then(response => {
+          setPokemons(prev => [...prev, response.data]);
+        });
+      }
+    }
   }, []);
 
 
-  function handleCurrent(e) {
-    if (Current.name !== e.name) {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${e.name}`).then(res => {
-        console.log(res.data);
-        setCurrent(res.data)
-      })
-    }
-  }
-
   return (
     <div className="App">
-
-      <div className="pokemon">
-        {Object.entries(Current).length > 0 ?
-          <div className="current">
-            <img src={Current.sprites.front_default} alt="" />
-          </div>
-          : "Selecione um pokemon"}
-
-      </div>
-
+      <h1>Pokedex</h1>
       <div className="list">
-        {Pokemons.length > 0 ? Pokemons.map((pokemon, index) => {
-          return (
-            <div className="card" key={index} onClick={() => { handleCurrent(pokemon) }}>
-              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt="" />
-              <h3>{index}</h3>
-              <h4>{pokemon.name}</h4>
-            </div>
-          )
-        }) : <h1>Pokedex vazia :(</h1>}
+        {Object.keys(Pokemons).length > 145 ? <PokemonList data={Pokemons} /> : 
+        <div className="loading"><img src={pokeball} alt="" /> <h1>Carregando pokemons</h1></div> }
       </div>
+
     </div>
   );
 }
+
+function PokemonList(props) {
+  props.data.sort((a, b) => {
+    if(a.id > b.id){return 1};
+    if(a.id < b.id){return -1};
+    return 0;
+  });
+  return (
+    <>
+      {props.data.map(pokemon => <PokemonCard key={props.data.id} data={pokemon} />)}
+    </>
+  )
+}
+function PokemonCard(props) {
+  return (
+    <div className="pokemon-card" style={{backgroundColor:`var(--${props.data.types[0].type.name})`}}>
+      {/* <img className="pokeball" src={pokeball} alt="" /> */}
+      <h4>{props.data.name}</h4>
+      <h5>{props.data.id}</h5>
+      <div className="type"><p>{props.data.types[0].type.name}</p></div>
+      <img src={props.data.sprites.front_default} alt="" />
+    </div>
+  )
+}
+
 
 
 export default App;
