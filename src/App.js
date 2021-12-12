7eec71b07c5a9aab05dd37eb6c9c740fd684509d
diogosamtos <diogo.santos@geowellex.com>
2021-12-12 02:14:09 -0300

    Refactoring list of pokemons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import './app.css';
+
 
 function App() {
+
+  const [Pokemons, setPokemons] = useState([]);
+  const [Current, setCurrent] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`).then(response => {
+      setPokemons(response.data.results);
+    });
+  }, []);
+
+
+  function handleCurrent(e) {
+    if (Current.name !== e.name) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${e.name}`).then(res => {
+        console.log(res.data);
+        setCurrent(res.data)
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="pokemon">
+        {Object.entries(Current).length > 0 ?
+          <div className="current">
+            <img src={Current.sprites.front_default} alt="" />
+          </div>
+          : "Selecione um pokemon"}
+
+      </div>
+
+      <div className="list">
+        {Pokemons.length > 0 ? Pokemons.map((pokemon, index) => {
+          return (
+            <div className="card" key={index} onClick={() => { handleCurrent(pokemon) }}>
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt="" />
+              <h3>{index}</h3>
+              <h4>{pokemon.name}</h4>
+            </div>
+          )
+        }) : <h1>Pokedex vazia :(</h1>}
+      </div>
     </div>
   );
 }
+
 
 export default App;
